b6db1d266e0c5e7ee7469f39cfb3f6b49e1ccebc
fed.lanza@gmail.com <fed.lanza@gmail.com@a0532354-6682-fca5-5541-cf62f0bcf979>
2012-01-21 05:13:53 +0000

    Agregue validaciones a los formularios.
    flanzani

--- trunk/WebProgramAR/WebProgramAR.Entidades/CursoPartial.cs
+++ trunk/WebProgramAR/WebProgramAR.Entidades/CursoPartial.cs
@@ -2,10 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebProgramAR.Entidades
 {
-    public partial class Ejercicio : EntidadProgramARBase
+    [MetadataType(typeof(CursoValidation))]
+    public partial class Curso : EntidadProgramARBase
     {
+    }
+
+
+
+    public class CursoValidation
+    {
+        [Display(Name="Nombre")]
+        [Required( ErrorMessage="Requerido")]
+        [StringLength(50, ErrorMessage = "El nombre de curso debe tener como maximo 50 caracteres")]
+        public string Nombre { get; set; }
     }
 }
--- trunk/WebProgramAR/WebProgramAR.Entidades/EjercicioPartial.cs
+++ trunk/WebProgramAR/WebProgramAR.Entidades/EjercicioPartial.cs
@@ -2,10 +2,34 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebProgramAR.Entidades
 {
+    [MetadataType(typeof(EjercicioValidation))]
     public partial class Ejercicio : EntidadProgramARBase
     {
+    }
+
+
+
+    public class EjercicioValidation
+    {
+        [Display(Name = "Nombre")]
+        [Required(ErrorMessage = "Requerido")]
+        [StringLength(50, ErrorMessage = "El nombre de ejercicio debe tener como maximo 50 caracteres")]
+        public string Nombre { get; set; }
+
+        [Display(Name = "Enunciado")]
+        [Required(ErrorMessage = "Requerido")]
+        public string Enunciado { get; set; }
+
+        [Display(Name = "Solucion")]
+        [Required(ErrorMessage = "Requerido")]
+        public string SolucionTexto { get; set; }
+
+        [Display(Name = "Nivel")]
+        [Required(ErrorMessage = "Requerido")]
+        public int NivelEjercicioId { get; set; }
     }
 }
