ea2d9b755a6d0a1113a246ab073c4b84a706cf3e
fed.lanza@gmail.com <fed.lanza@gmail.com@a0532354-6682-fca5-5541-cf62f0bcf979>
2012-03-01 04:02:01 +0000

    segu por valor. el comienzo

---
+++ WebProgramAR/WebProgramAR.DataAccess/ReglasSeguridadDA.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WebProgramAR.Entidades;
+using System.Data;
+using WebProgramAR.DataAccess.Interfases;
+
+namespace WebProgramAR.DataAccess
+{
+    public class ReglasSeguridadDA
+    {     
+        public static IEnumerable<ReglasSeguridad> GetReglasByTablaByUsuarioByTipoUsuario(string tablaNombre, int? userId, int? tipoUserId)
+        {
+           try
+           {
+               using (WebProgramAREntities db = new WebProgramAREntities())
+                {
+                    IQueryable<ReglasSeguridad> query = from u in db.ReglasSeguridads.Include("Tabla").Include("Columna").Include("Comparador")
+                                                        where (   
+                                                            u.Tabla.Nombre.ToUpper() == tablaNombre.ToUpper() && 
+                                                            u.UsuarioId == userId &&  
+                                                            u.TipoUsuarioId == tipoUserId 
+                                                        )
+                                                        select u;
+                    return query.ToList();
+                }
+            }
+           catch (Exception ex)
+           {
+               throw new Exception(ex.Message.ToString());
+           }
+        }
+
+        public static IEnumerable<Provincia> GetProvincias()
+        {
+            try
+            {
+                using (WebProgramAREntities db = new WebProgramAREntities())
+                {
+                    IQueryable<Provincia> query = from u in db.Provincias
+                                                  select u;
+                    return query.ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message.ToString());
+            }
+        }
+
+      
+    }
+}
---
+++ WebProgramAR/WebProgramAR.DataAccess/Seguridad/SeguridadXValorManager.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WebProgramAR.Entidades;
+using System.Reflection;
+
+namespace WebProgramAR.DataAccess.Seguridad
+{
+    public static class SeguridadXValorManager
+    {
+
+        public static List<EntidadProgramARBase> Filtrar(List<EntidadProgramARBase> lista, string tabla, int? userId, int? tipoUserId)
+        {
+            List<EntidadProgramARBase> retorno = new List<EntidadProgramARBase>();
+
+            List<ReglasSeguridad> reglas = ReglasSeguridadDA.GetReglasByTablaByUsuarioByTipoUsuario(tabla, userId, tipoUserId).ToList();
+
+            if (lista.Count > 0)
+            {
+                PropertyInfo[] props = lista[0].GetType().GetProperties();
+
+                foreach (var entidad in lista)
+                {
+                    bool resGlobalTodasReglas = true;
+
+                    foreach (ReglasSeguridad regla in reglas)
+                    {
+                        PropertyInfo prop = props.Single(x => x.Name == ""); //aca va la columna
+
+                        bool resLocal;
+
+                        switch (regla.Tabla.Nombre) //Aca va el tipo de la col
+                        {
+                            default:
+                                object obj = prop.GetValue(entidad, null);
+                                resLocal = Comparar((string)obj, regla.Comparador.Nombre, regla.Valor);
+                                break;
+                        }                        
+
+                        resGlobalTodasReglas &= resLocal;
+
+                        if (!resGlobalTodasReglas)
+                        {
+                            break;
+                        }
+                    }
+
+                    if (resGlobalTodasReglas)
+                    {
+                        retorno.Add(entidad);
+                    }
+                }
+            }
+
+            return retorno;
+        }
+
+        private static bool Comparar(string valEntidad, string comp, string valRef)
+        {
+            switch (comp)
+            { 
+                case ConstantesSeguridad.IGUAL_NOCASESENSITIVE:
+                    return string.Compare(valEntidad,valRef,true) == 0;
+                case ConstantesSeguridad.DISTINTO_NOCASESENSITIVE:
+                    return string.Compare(valEntidad,valRef,true) != 0;
+                case ConstantesSeguridad.EMPIEZA_CON_NOCASESENSITIVE:
+                    return valEntidad.ToUpper().StartsWith(valRef.ToUpper());
+                case ConstantesSeguridad.TERMINA_CON_NOCASESENSITIVE:
+                    return valEntidad.ToUpper().EndsWith(valRef.ToUpper());
+                default:
+                    break;
+            }
+            return true;
+        }
+
+        private static bool Comparar(int valEntidad, string comp, int valRef)
+        {
+            switch (comp)
+            {
+                case ConstantesSeguridad.MAYOR_IGUAL:
+                    return valEntidad >= valRef;
+                case ConstantesSeguridad.MENOR_IGUAL:
+                    return valEntidad <= valRef;
+                case ConstantesSeguridad.MAYOR:
+                    return valEntidad > valRef;
+                case ConstantesSeguridad.MENOR:
+                    return valEntidad < valRef;
+                case ConstantesSeguridad.IGUAL:
+                    return valEntidad == valRef;
+                case ConstantesSeguridad.DISTINTO:
+                    return valEntidad != valRef;
+                default:
+                    break;
+            }
+            return true;
+        }
+    }
+}
