ec94d3f23af7bf82c66fb307d3e4c0ed8422e328
fed.lanza@gmail.com <fed.lanza@gmail.com@a0532354-6682-fca5-5541-cf62f0bcf979>
2012-03-01 04:02:01 +0000

    segu por valor. el comienzo

---
+++ trunk/WebProgramAR/WebProgramAR.DataAccess/Seguridad/ConstantesSeguridad.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WebProgramAR.DataAccess.Seguridad
+{
+    public static class ConstantesSeguridad
+    {
+        //Comparadores
+        //enteros
+        public const string MENOR_IGUAL = "<=";
+        public const string MENOR = "<";
+        public const string MAYOR_IGUAL = ">=";
+        public const string MAYOR = ">";
+        public const string IGUAL = "=";
+        public const string DISTINTO = "!=";
+
+        //string
+        public const string EMPIEZA_CON_NOCASESENSITIVE = "LIKE '{0}%'";
+        public const string TERMINA_CON_NOCASESENSITIVE = "LIKE '%{0}'";
+        public const string IGUAL_NOCASESENSITIVE = "LIKE '{0}'";
+        public const string DISTINTO_NOCASESENSITIVE = "NOT LIKE '{0}'";
+        
+        
+
+    }
+}
