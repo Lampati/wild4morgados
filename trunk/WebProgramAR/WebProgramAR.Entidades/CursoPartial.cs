57e58fb5f4eef393e08f3f1af94d932d65fbcd36
fed.lanza@gmail.com <fed.lanza@gmail.com@a0532354-6682-fca5-5541-cf62f0bcf979>
2012-10-08 17:16:19 +0000

    Agregados metodos de clonacion para cada una de las entidades. En GetCursoById se reemplazo y se clona el curso antes de devolverlo para que no tire ObjectDisposedException al intentar acceder a sus ejercicios.

--- trunk/WebProgramAR/WebProgramAR.Entidades/CursoPartial.cs
+++ trunk/WebProgramAR/WebProgramAR.Entidades/CursoPartial.cs
@@ -7,8 +7,42 @@ using System.ComponentModel.DataAnnotations;
 namespace WebProgramAR.Entidades
 {
     [MetadataType(typeof(CursoValidation))]
-    public partial class Curso : EntidadProgramARBase
+    public partial class Curso : EntidadProgramARBase, ICloneable
     {
+
+        
+
+        #region ICloneable Members
+
+        public object Clone()
+        {
+            Curso copia = new Curso();
+
+            copia.CursoId = this.CursoId;
+            copia.Nombre = this.Nombre;
+
+            if (this.Usuario != null)
+            {
+                copia.Usuario = this.Usuario.Clone() as Usuario;              
+            }
+
+            if (this.Ejercicios != null)
+            {
+                copia.Ejercicios = new List<Ejercicio>();
+
+                foreach (var item in this.Ejercicios.ToList())
+                {
+                    Ejercicio ej = item.Clone() as Ejercicio;               
+
+
+                    copia.Ejercicios.Add(ej);
+                }
+            }
+
+            return copia;
+        }
+
+        #endregion
     }
 
 
--- trunk/WebProgramAR/WebProgramAR.Entidades/UsuarioPartial.cs
+++ trunk/WebProgramAR/WebProgramAR.Entidades/UsuarioPartial.cs
@@ -10,10 +10,59 @@ namespace WebProgramAR.Entidades
 {
     
     [MetadataType(typeof(UsuarioValidation))]
-    public partial class Usuario : EntidadProgramARBase
+    public partial class Usuario : EntidadProgramARBase, ICloneable
     {
         public string Email { get; set; }
 
+
+        #region ICloneable Members
+
+        public object Clone()
+        {
+            Usuario copia = new Usuario();
+            copia.UsuarioId = this.UsuarioId;
+            copia.UsuarioNombre = this.UsuarioNombre;
+            copia.TipoUsuarioId = this.TipoUsuarioId;
+            copia.Apellido = this.Apellido;
+            copia.Nombre = this.Nombre;
+            copia.Email = this.Email;
+            copia.LocalidadId = this.LocalidadId;
+            copia.PaisId = this.PaisId;
+            copia.ProvinciaId = this.ProvinciaId;
+            copia.FechaAlta = this.FechaAlta;
+
+            if (this.TipoUsuario != null)
+            {
+                copia.TipoUsuario = new TipoUsuario();
+                copia.TipoUsuario.TipoUsuarioId = this.TipoUsuario.TipoUsuarioId;
+                copia.TipoUsuario.Descripcion = this.TipoUsuario.Descripcion;
+            }
+
+            if (this.Pais != null)
+            {
+                copia.Pais = new Pais();
+                copia.Pais.PaisId = this.Pais.PaisId;
+                copia.Pais.Descripcion = this.Pais.Descripcion;
+            }
+
+            if (this.Provincia != null)
+            {
+                copia.Provincia = new Provincia();
+                copia.Provincia.ProvinciaId = this.Provincia.ProvinciaId;
+                copia.Provincia.Descripcion = this.Provincia.Descripcion;
+            }
+
+            if (this.Localidad != null)
+            {
+                copia.Localidad = new Localidad();
+                copia.Localidad.LocalidadId = this.Localidad.LocalidadId;
+                copia.Localidad.Descripcion = this.Localidad.Descripcion;
+            }
+
+            return copia;
+        }
+
+        #endregion
     }
 
 
