fef393e21bac452099f9a689ed30224540cffe46
ezetol@gmail.com <ezetol@gmail.com@a0532354-6682-fca5-5541-cf62f0bcf979>
2012-11-22 02:26:12 +0000

    Cascara del Ejer-Browser

---
+++ trunk/WebProgramAR/WebProgramAR.WebService/EntidadesDTO/CursoDetalleDTO.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebProgramAR.WebService.EntidadesDTO
+{
+    public class CursoDetalleDTO
+    {
+        #region Atributos
+        private int cursoId;
+        private List<EjercicioDetalleDTO> ejercicios;
+        #endregion
+
+        #region Propiedades
+        public int CursoId
+        {
+            get { return this.cursoId; }
+            set { this.cursoId = value; }
+        }
+
+        public List<EjercicioDetalleDTO> Ejercicios
+        {
+            get { return this.ejercicios; }
+            set { this.ejercicios = value; }
+        }
+        #endregion
+
+        #region Constructores
+        public CursoDetalleDTO() { }
+
+        public CursoDetalleDTO(int cursoId)
+        {
+            this.cursoId = cursoId;
+            this.ejercicios = new List<EjercicioDetalleDTO>();
+
+            Random r = new Random();
+            for (int i = 0; i < r.Next(0, 11); i++)
+                this.ejercicios.Add(new EjercicioDetalleDTO(i + 1));
+        }
+        #endregion
+    }
+}
